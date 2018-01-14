596e62e04aeaa74d838a68ab1bdb6a59ea5adff7
udit kumar <uditkumar489@gmail.com>
2018-01-14 09:50:48 +0530

    initial commit
    necessary packages added

---
+++ classify.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 14 09:46:58 2018
+
+@author: Udit
+"""
+# Convolutional Neural Network
+
+# IMPORTANT
+# Please intall : {1.Theano  ,  2.Tensorflow  ,  3.Keras}  before executing it.
+
+# Importing the required libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
