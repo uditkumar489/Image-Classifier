e31579b90fc911e4ad12fb8543402826098e53ed
udit kumar <uditkumar489@gmail.com>
2018-01-14 10:08:41 +0530

    dataset structuring
    The required structure of folders is loaded

--- classify.py
+++ classify.py
@@ -5,6 +5,7 @@ Created on Sun Jan 14 09:46:58 2018
 IMPORTANT :
     1. This project is for a specific type of folder arrangement of dataset
     2. Please intall : {1.Theano  ,  2.Tensorflow  ,  3.Keras}  before executing it.
+    3. Replace the folder names according to your own requirements
     
 NOTE : Dataset is empty but structured ; use your own
 @author: Udit
@@ -18,3 +19,13 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+
+"""
+Building a CNN requires the following steps :
+    1. Convolution
+    2. Pooling
+    3. Flattening
+    4. Full connection
+    5. Compiling
+"""
